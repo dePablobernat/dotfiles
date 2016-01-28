8d2b89998e1e6558aa081c8776295c1342ab4575
Alberto Fernandez <(null)>
2016-01-28 03:02:42 +0100

    First commit

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXTRA_DIR="$HOME/.extra"
+
+# Update dotfiles itself first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin maste
+
+# Bunch of symlinks
+for DOTFILE in "$DOTFILES_DIR"/{runcom,git}/.*; do
+  if [ -f ~/${DOTFILE##*/} ]; then
+    if [ -f ~/${DOTFILE##*/}.bk ]; then
+      read -n 1 -p "Backup file ~/${DOTFILE##*/}.bk already exists, do you want to override it?[Y/n]" OVERRIDE_BK
+      if [[ $OVERRIDE_BK == {"y","Y",""} ]]; then
+        mv -f ~/${DOTFILE##*/} ~/${DOTFILE##*/}.bk
+        echo "~/${DOTFILE##*/} overrided ~/${DOTFILE##*/}.bk"
+      fi
+    else
+      mv ~/${DOTFILE##*/} ~/${DOTFILE##*/}.bk
+      echo "~/${DOTFILE##*/} moved to ~/${DOTFILE##*/}.bk"
+    fi
+  fi
+
+  if [ -f "$DOTFILE" ]; then
+    if [ "$OS" = "MINGW64_NT-10.0" ]; then
+      PARSED_HOME=${HOME//\//\\}
+      PARSED_HOME=${PARSED_HOME/\\/}
+      PARSED_HOME=${PARSED_HOME/\\/:\\}
+      PARSED_DOTF=${DOTFILE//\//\\}
+      PARSED_DOTF=${PARSED_DOTF/\\/}
+      PARSED_DOTF=${PARSED_DOTF/\\/:\\}
+      [ -f ~/${DOTFILE##*/} ] && rm ~/${DOTFILE##*/}
+      cmd //C mklink $PARSED_HOME\\${DOTFILE##*/} $PARSED_DOTF
+      unset PARSED_HOME
+      unset PARSED_DOTF
+    else
+      ln -sfv "$DOTFILE" ~
+    fi
+    echo "linked ${DOTFILE##*/}"
+  fi
+done
